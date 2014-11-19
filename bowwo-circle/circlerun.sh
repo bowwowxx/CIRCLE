eaed7d3a8a57a52b5383109b1c375e9bad19fe36
bowwowxx <jummykimo@gmail.com>
2014-11-19 18:04:16 +0800

    no message

--- bowwo-circle/circlerun.sh
+++
@@ -1,1 +0,0 @@
-docker run --name circle  -d -p 8080:80 -p 443:443 circle/wordpress 
--- bowwo-circle/sendfile.sh
+++
@@ -1,6 +0,0 @@
-name=$(docker inspect -f '{{.Id}}' circle)
-echo $name
-sudo cp *.sql /var/lib/docker/btrfs/subvolumes/$name/tmp/
-sudo cp load.sh /var/lib/docker/btrfs/subvolumes/$name/tmp/load.sh
-PID=`docker  inspect  --format "{{ .State.Pid }}" circle `
-sudo nsenter --target $PID --mount --uts --ipc --net --pid
